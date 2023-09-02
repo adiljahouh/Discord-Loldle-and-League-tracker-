2476c0b6d2bbc932189ba918d94e731c560843d1
MSchotsman <38855941+MSchotsman@users.noreply.github.com>
2023-09-02 20:50:48 +0200

    Add init files for each module

---
+++ src/commands/__init__.py
@@ -0,0 +1,6 @@
+from .animal_commands import AnimalCommands
+from .commands_utility import role_check, mod_check
+from .league_commands import LeagueCommands
+from .loop import loops
+from .point_commands import PointCommands
+from .utility.team_image import imageCreator
---
+++ src/databases/__init__.py
@@ -0,0 +1,2 @@
+from .betting_db import BettingDB
+from .main_db import MainDB
