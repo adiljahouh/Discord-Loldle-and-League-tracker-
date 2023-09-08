5d4c9ec06da70401487ed59317a828cd07378264
MSchotsman <38855941+MSchotsman@users.noreply.github.com>
2023-09-08 11:32:29 +0200

    Image improvements

--- src/commands/utility/end_image.py
+++ src/commands/utility/end_image.py
@@ -69,7 +69,11 @@ class EndImage():
         team["players"].append({"name": player["summonerName"], "champ_id": player["championId"],
                                 "champ_name": player["championName"],
                                 "damage_dealt": player["totalDamageDealtToChampions"],
-                                "damage_taken": player["totalDamageTaken"]})
+                                "damage_taken": player["totalDamageTaken"],
+                                "kills": player["kills"],
+                                "deaths": player["deaths"],
+                                "assists": player["assists"]
+                                })
 
     async def get_team_image(self):
         # Define colors
@@ -77,8 +81,9 @@ class EndImage():
         col_green = (6, 226, 191)
         col_white = (255, 255, 255)
         col_gray = (60, 60, 60)
+        col_black = (0, 0, 0)
         # Define fonts
-        font_xll = ImageFont.truetype('../assets/Social Gothic Bold.otf', 80)
+        font_xll = ImageFont.truetype('../assets/Social Gothic Bold.otf', 90)
         font_big = ImageFont.truetype('../assets/Social Gothic Bold.otf', 49)
         font_small = ImageFont.truetype('../assets/Social Gothic Bold.otf', 25)
         font_text_middle = ImageFont.truetype('../assets/nimbussannovt.ttf', 35)
@@ -96,10 +101,10 @@ class EndImage():
             color = col_green
         # Show gameresult
         _, _, w, h = draw_text.textbbox((0, 0), f"Jeroen {result}", font=font_xll)
-        draw_text.text((105+(775-w)/2, (190-h)/2), f"Jeroen {result}", font=font_xll, fill=color)
+        draw_text.text((105+(775-w)/2, (243-h)/2), f"Jeroen {result}", font=font_xll, fill=color)
         # Show gametime
-        _, _, w, h = draw_text.textbbox((0, 0), f"Gametime {self.game_time}", font=font_small)
-        draw_text.text((105+(775-w)/2, 140+(134-h)/2), f"Gametime {self.game_time}", font=font_small, fill=col_white)
+        _, _, w, h = draw_text.textbbox((0, 0), f"Gametime {self.game_time}", font=font_text_middle)
+        draw_text.text((105+(775-w)/2, 180+(134-h)/2), f"Gametime {self.game_time}", font=font_text_middle, fill=col_white)
         # Show middle text
         for indx, attr in enumerate(['K/D/A', 'GOLD', 'TOWERS', 'DRAKES', 'ELDER DRAGONS', 'BARONS', 'BANS']):
             _, _, w, h = draw_text.textbbox((0, 0), attr, font=font_text_middle)
@@ -148,25 +153,28 @@ class EndImage():
         for team_indx, team in enumerate(self.teams):
             for indx, player in enumerate(team["players"]):
                 if team_indx == 0:
-                    base_image.paste(col_green, (960, 40 + (205*indx), 1150 + int(250*(player['damage_dealt'] - min_stat)/(max_stat - min_stat)), 100 + (205*indx)))
-                    base_image.paste(col_gray, (960, 140 + (205*indx), 1150 + int(250*(player['damage_taken'] - min_stat)/(max_stat - min_stat)), 200 + (205*indx)))
+                    base_image.paste(col_green, (960, 30 + (205*indx), 1150 + int(250*(player['damage_dealt'] - min_stat)/(max_stat - min_stat)), 100 + (205*indx)))
+                    base_image.paste(col_gray, (960, 140 + (205*indx), 1150 + int(250*(player['damage_taken'] - min_stat)/(max_stat - min_stat)), 210 + (205*indx)))
                 else:
-                    base_image.paste(col_green, (1665 - int(250*(player['damage_dealt'] - min_stat)/(max_stat - min_stat)), 40 + (205*indx), 1855, 100 + (205*indx)))
-                    base_image.paste(col_gray, (1665 - int(250*(player['damage_taken'] - min_stat)/(max_stat - min_stat)), 140 + (205*indx), 1855, 200 + (205*indx)))
+                    base_image.paste(col_red, (1665 - int(250*(player['damage_dealt'] - min_stat)/(max_stat - min_stat)), 30 + (205*indx), 1855, 100 + (205*indx)))
+                    base_image.paste(col_gray, (1665 - int(250*(player['damage_taken'] - min_stat)/(max_stat - min_stat)), 140 + (205*indx), 1855, 210 + (205*indx)))
 
         # Player names, damage numbers, and player champ images
         for team_indx, team in enumerate(self.teams):
             for indx, player in enumerate(team["players"]):
+                kda = f"{player['kills']}/{player['deaths']}/{player['assists']}"
                 _, _, w, h = draw_text.textbbox((0, 0), str(player['name']), font=font_small)
-                draw_text.text((1090 + (635*team_indx) - (w*team_indx), 60 + (205*indx) + (120-h)/2), str(player['name']), font=font_small, fill=col_white)
+                draw_text.text((1090 + (635*team_indx) - (w*team_indx), 60 + (205*indx) + (120-h)/2), str(player['name']), font=font_small, fill=col_white, stroke_width=2, stroke_fill=col_black)
                 _, _, w, h = draw_text.textbbox((0, 0), str(player['damage_dealt']), font=font_small)
-                draw_text.text((1090 + (635*team_indx) - (w*team_indx), 10 + (205*indx) + (120-h)/2), str(player['damage_dealt']), font=font_small, fill=col_white)
+                draw_text.text((1090 + (635*team_indx) - (w*team_indx), 5 + (205*indx) + (120-h)/2), str(player['damage_dealt']), font=font_small, fill=col_white, stroke_width=2, stroke_fill=col_black)
                 _, _, w, h = draw_text.textbbox((0, 0), str(player['damage_taken']), font=font_small)
-                draw_text.text((1090 + (635*team_indx) - (w*team_indx), 110 + (205*indx) + (120-h)/2), str(player['damage_taken']), font=font_small, fill=col_white)
+                draw_text.text((1090 + (635*team_indx) - (w*team_indx), 115 + (205*indx) + (120-h)/2), str(player['damage_taken']), font=font_small, fill=col_white, stroke_width=2, stroke_fill=col_black)
                 champ_image: Image = await champion_splash(champ_list[str(player['champ_id'])])
                 champ_image: Image = champ_image.convert('RGBA')
                 pos = (960 + (775*team_indx), 55 + (205*indx))
                 base_image.paste(champ_image, pos, champ_image)
+                _, _, w, h = draw_text.textbbox((0, 0), kda, font=font_small)
+                draw_text.text((960 + (775*team_indx) + (120-w)/2, 180 + (205*indx)), kda, font=font_small, fill=col_white, stroke_width=2, stroke_fill=col_black)
 
 
 
