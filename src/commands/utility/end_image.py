56648dbc85bcbe74776c32d0f6d61c1ae2acc37c
MSchotsman <38855941+MSchotsman@users.noreply.github.com>
2023-09-09 20:28:05 +0200

    Refactor end_image and added comments

--- src/commands/utility/end_image.py
+++ src/commands/utility/end_image.py
@@ -5,47 +5,48 @@ from api.ddragon import get_champion_list, champion_splash
 from PIL import Image, ImageDraw, ImageFont
 
 
-class EndImage():
+def team_dict():
+    return {
+        "bans": [], "players": [], "kills": 0, "deaths": 0, "assists": 0,
+        "gold": 0, "towers": 0, "drakes": 0, "elder": 0, "baron": 0
+    }
+
+
+def img_to_bytes(image: Image):
+    byte_arr = BytesIO()
+    image.save(byte_arr, format="PNG")
+    byte_arr.seek(0)
+    return byte_arr
+
+
+class EndImage:
 
     def __init__(self, data, name):
         self.data = data
         self.name = name
         self.puuid = 0
         self.player_team_id = 100
-        self.swap = False
         self.won_team_id = 100
-        self.team_one = self.fill_barely()
-        self.team_two = self.fill_barely()
+        self.team_one = team_dict()
+        self.team_two = team_dict()
         self.teams = [self.team_one, self.team_two]
         self.game_time = 0
         self.prepare_data()
 
-    def print_data(self):
-        for team in self.teams:
-            for attribute in team.keys():
-                if attribute == "players":
-                    for val in team[attribute]:
-                        print(val)
-                    continue
-                print(attribute, team[attribute])
-            print("-"*20)
-
-
-    def fill_barely(self):
-        return {"bans": [], "players": [], "kills": 0, "deaths": 0, "assists": 0, "gold": 0, "towers": 0, "drakes": 0, "elder": 0, "baron": 0}
-
     def prepare_data(self):
-        # First find puuid
+        # Get puuid to place main player in team
         for player in self.data["info"]["participants"]:
             if player["summonerName"].lower() == self.name.lower():
                 self.puuid = player["puuid"]
         player_indx = self.data["metadata"]['participants'].index(self.puuid)
         if player_indx >= 5:
             self.player_team_id = 200
+        # Set time
         self.game_time = int(self.data["info"]["gameDuration"])
         seconds = self.game_time % 60
         seconds = seconds if seconds > 10 else f"0{seconds}"
         self.game_time = f"{int((self.game_time/60) // 1)}:{seconds}"
+        # Set team results
         for indx, team in enumerate(self.data["info"]["teams"]):
             for ban in team["bans"]:
                 self.teams[indx]["bans"].append(ban["championId"])
@@ -54,21 +55,22 @@ class EndImage():
             self.teams[indx]["towers"] = objectives["tower"]["kills"]
             self.teams[indx]["drakes"] = objectives["dragon"]["kills"]
             self.teams[indx]["baron"] = objectives["baron"]["kills"]
+        # Determine result of main player
         if self.data["info"]["teams"][0]["win"]:
             self.won_team_id = 100
         else:
             self.won_team_id = 200
         for indx, player in enumerate(self.data["info"]["participants"]):
             if indx < 5:
-                self.missing_data(self.team_one, player)
+                self.fill_player_info(self.team_one, player)
             else:
-                self.missing_data(self.team_two, player)
+                self.fill_player_info(self.team_two, player)
+        # Swap the teams if main player is not in the first team
         if self.player_team_id != 100:
             self.team_one, self.team_two = self.team_two, self.team_one
             self.teams = [self.team_one, self.team_two]
 
-
-    def missing_data(self, team, player):
+    def fill_player_info(self, team, player):
         team["elder"] = player["challenges"]["teamElderDragonKills"]
         team["assists"] += player["assists"]
         team["deaths"] += player["deaths"]
@@ -94,17 +96,17 @@ class EndImage():
         col_gray = (60, 60, 60)
         col_black = (0, 0, 0)
         # Define fonts
-        font_xll = ImageFont.truetype('../assets/image_generator/Social Gothic Bold.otf', 90)
-        font_big = ImageFont.truetype('../assets/image_generator/Social Gothic Bold.otf', 49)
-        font_small = ImageFont.truetype('../assets/image_generator/Social Gothic Bold.otf', 25)
-        font_text_middle = ImageFont.truetype('../assets/image_generator/nimbussannovt.ttf', 35)
+        font_xll = ImageFont.truetype('./assets/image_generator/Social Gothic Bold.otf', 90)
+        font_big = ImageFont.truetype('./assets/image_generator/Social Gothic Bold.otf', 50)
+        font_small = ImageFont.truetype('./assets/image_generator/Social Gothic Bold.otf', 25)
+        font_text_middle = ImageFont.truetype('./assets/image_generator/nimbussannovt.ttf', 35)
 
         base_image = Image.new(mode="RGB", size=(1920, 1080))
-        img = Image.open('../assets/image_generator/end_image.png')
+        img = Image.open('./assets/image_generator/end_image.png')
         img = img.convert('RGBA')
         base_image.paste(img, (0, 0), img)
-
         draw_text = ImageDraw.Draw(base_image)
+
         result = "Lost"
         color = col_red
         if self.player_team_id == self.won_team_id:
@@ -120,19 +122,14 @@ class EndImage():
         for indx, attr in enumerate(['K/D/A', 'GOLD', 'TOWERS', 'DRAKES', 'ELDER DRAGONS', 'BARONS', 'BANS']):
             _, _, w, h = draw_text.textbbox((0, 0), attr, font=font_text_middle)
             draw_text.text((105 + (776-w)/2, 290+(indx*100)+(100-h)/2), attr, font=font_text_middle, fill=col_white)
-        # Might be able to loop from here
-        team = self.teams[0]
-        _, _, w, h = draw_text.textbbox((0, 0), f"{team['kills']}/{team['deaths']}/{team['assists']}", font=font_big)
-        draw_text.text((105, 290+(100-h)/2), f"{team['kills']}/{team['deaths']}/{team['assists']}", font=font_big, fill=col_white)
-        for indx, attr in enumerate(['gold', 'towers', 'drakes', 'elder', 'baron']):
-            _, _, w, h = draw_text.textbbox((0, 0), str(team[attr]), font=font_big)
-            draw_text.text((105, 390+(100*indx)+(100-h)/2), str(team[attr]), font=font_big, fill=col_white)
-        team = self.teams[1]
-        _, _, w, h = draw_text.textbbox((0, 0), f"{team['kills']}/{team['deaths']}/{team['assists']}", font=font_big)
-        draw_text.text((880-w, 290+(100-h)/2), f"{team['kills']}/{team['deaths']}/{team['assists']}", font=font_big, fill=col_white)
-        for indx, attr in enumerate(['gold', 'towers', 'drakes', 'elder', 'baron']):
-            _, _, w, h = draw_text.textbbox((0, 0), str(team[attr]), font=font_big)
-            draw_text.text((880-w, 390+(100*indx)+(100-h)/2), str(team[attr]), font=font_big, fill=col_white)
+
+        # Show team stats
+        for team_indx, team in enumerate(self.teams):
+            _, _, w, h = draw_text.textbbox((0, 0), f"{team['kills']}/{team['deaths']}/{team['assists']}", font=font_big)
+            draw_text.text((105 + team_indx*(775-w), 290+(100-h)/2), f"{team['kills']}/{team['deaths']}/{team['assists']}", font=font_big, fill=col_white)
+            for indx, attr in enumerate(['gold', 'towers', 'drakes', 'elder', 'baron']):
+                _, _, w, h = draw_text.textbbox((0, 0), str(team[attr]), font=font_big)
+                draw_text.text((105 + team_indx*(775-w), 390+(100*indx)+(100-h)/2), str(team[attr]), font=font_big, fill=col_white)
 
         # Bans
         champ_list = await get_champion_list()
@@ -162,6 +159,7 @@ class EndImage():
                     min_stat = lowest
                 if highest > max_stat:
                     max_stat = highest
+
         # Draw damage graph
         for team_indx, team in enumerate(self.teams):
             for indx, player in enumerate(team["players"]):
@@ -192,12 +190,12 @@ class EndImage():
                 base_image.paste(champ_image, pos, champ_image)
                 if player["hero"]:
                     if self.won_team_id == self.player_team_id:
-                        img = Image.open('../assets/image_generator/crown.png')
+                        img = Image.open('./assets/image_generator/crown.png')
                         img = img.rotate(25)
                         img = img.resize((75, 75))
                         base_image.paste(img.convert('RGB'), (920 + (775*team_indx), 15 + (205*indx)), img.convert('RGBA'))
                     else:
-                        img = Image.open('../assets/image_generator/dunce.png')
+                        img = Image.open('./assets/image_generator/dunce.png')
                         img = img.rotate(35)
                         img = img.resize((75, 75))
                         base_image.paste(img.convert('RGB'), (925 + (775*team_indx), 15 + (205*indx)), img.convert('RGBA'))
@@ -205,34 +203,5 @@ class EndImage():
                 # Player kda
                 _, _, w, h = draw_text.textbbox((0, 0), kda, font=font_small)
                 draw_text.text((960 + (775*team_indx) + (120-w)/2, 180 + (205*indx)), kda, font=font_small, fill=col_white, stroke_width=2, stroke_fill=col_black)
-        return self.img_to_bytes(base_image)
         # base_image.show()
-
-    def img_to_bytes(self, image: Image):
-        byte_arr = BytesIO()
-        image.save(byte_arr, format="PNG")
-        byte_arr.seek(0)
-        return byte_arr
-
-
-
-    # for team_num, team in enumerate(self.champions):
-        #     for champ_num, champ in enumerate(team):
-        #         champ_image: Image = await self.champion_splash(champ)
-        #         champ_image = champ_image.convert('RGBA')
-        #         position = (70+(520*team_num), 110+(170*champ_num))
-        #         base_image.paste(champ_image, position, champ_image)
-        #         position = (position[0]+130, position[1]+40)
-        #         draw_text.text(position, self.players[team_num][champ_num], fill=(255, 255, 255), font=myFont)
-        # bet_text_upper = "BETTING HAS STARTED"
-        # bet_text_lower = ".bet <win/lose> <amount>"
-        # _, _, w, h = draw_text.textbbox((0, 0), bet_text_upper, font=myFont)
-        # draw_text.text(((1100-w)/2, 925+(50-h)/2), bet_text_upper, font=myFont, fill=(255, 255, 255))
-        # _, _, w, h = draw_text.textbbox((0, 0), bet_text_lower, font=myFont)
-        # draw_text.text(((1100-w)/2, 955+(50-h)/2), bet_text_lower, font=myFont, fill=(255, 255, 255))
-        # myFont = ImageFont.truetype('/assets/Gidole-Regular.ttf', 50)
-        # _, _, w, h = draw_text.textbbox((0, 0), self.game_mode, font=myFont)
-        # draw_text.text(((1100-w)/2, (100-h)/2), self.game_mode, font=myFont, fill=(255, 255, 255))
-        # return self.img_to_bytes(base_image)
-
-
+        return img_to_bytes(base_image)
--- src/main_end_image.py
+++ src/main_end_image.py
@@ -1,6 +1,6 @@
 import aiohttp
 from config import Settings
-from src.api.riot import riotAPI
+from api.riot import riotAPI
 from commands.utility.end_image import EndImage
 import asyncio
 
@@ -11,15 +11,15 @@ async def main():
     params = {
         "api_key": settings.RIOTTOKEN
     }
-    puuid = await client.get_puuid("nightlon")
-    match = (await client.get_match_ids("puuid", puuid, count=5, queue_id=420))[3]
+    user = "nightlon"
+    puuid = await client.get_puuid(user)
+    match = (await client.get_match_ids("puuid", puuid, count=5))[0] #, queue_id=420
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://europe.api.riotgames.com/lol/match/v5/matches/{match}", params=params) as response:
             response.raise_for_status()
             content = await response.json()
             data = content
-    end = EndImage(data, puuid)
-    end.print_data()
+    end = EndImage(data, user)
     await end.get_team_image()
 
 
