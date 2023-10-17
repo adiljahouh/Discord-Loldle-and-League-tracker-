a6b1b68348bed7d424bb119a4817fdef4cafb6d9
adiljahouh <a_jahouh@hotmail.com>
2023-10-17 17:16:56 +0200

    try except the leaderboard command

--- src/commands/point_commands.py
+++ src/commands/point_commands.py
@@ -165,31 +165,35 @@ class PointCommands(commands.Cog):
         """
             Returns point leaderboard with pagination support
         """
-        print("Leaderboard command")
-        leaderboard = None
-        page_number = 1
-        if len(args) == 0:
-            leaderboard = self.main_db.get_all_users_sorted_by_field("points", True, 0, 9)
-        else:
-            try:
-                page_number = int(args[0])
-                if page_number <= 0:
+        try:
+            print("Leaderboard command")
+            leaderboard = None
+            page_number = 1
+            if len(args) == 0:
+                leaderboard = self.main_db.get_all_users_sorted_by_field("points", True, 0, 9)
+            else:
+                try:
+                    page_number = int(args[0])
+                    if page_number <= 0:
+                        await ctx.send("Specify a whole number larger than 0")
+                        return
+                    page_size = 10
+                    start = (page_number - 1) * page_size
+                    end = start + page_size - 1
+                    leaderboard = self.main_db.get_all_users_sorted_by_field("points", True, start, end)
+                except ValueError:
                     await ctx.send("Specify a whole number larger than 0")
                     return
-                page_size = 10
-                start = (page_number - 1) * page_size
-                end = start + page_size - 1
-                leaderboard = self.main_db.get_all_users_sorted_by_field("points", True, start, end)
-            except ValueError:
-                await ctx.send("Specify a whole number larger than 0")
-                return
-        leaderboard_text = ''
-        for index, user in enumerate(leaderboard):
-            leaderboard_text += f'\n{index + 1 + ((page_number * 10) - 10)}. <@{user[0]}> | {user[1]} points'
-        description = f"99 percent of gamblers quit right before they hit it big! \n This is page {page_number}, to look at the next page use '.leaderboard {page_number+1}'"
-        embed = discord.Embed(title="Biggest gambling addicts 🃏\n\n", description=f"{description}", color=0xFF0000)
-        embed.add_field(name="Top 10 point havers on the server", value=leaderboard_text)
-        await ctx.send(embed=embed)
+            leaderboard_text = ''
+            for index, user in enumerate(leaderboard):
+                leaderboard_text += f'\n{index + 1 + ((page_number * 10) - 10)}. <@{user[0]}> | {user[1]} points'
+            description = f"99 percent of gamblers quit right before they hit it big! \n This is page {page_number}, to look at the next page use '.leaderboard {page_number+1}'"
+            embed = discord.Embed(title="Biggest gambling addicts 🃏\n\n", description=f"{description}", color=0xFF0000)
+            embed.add_field(name="Top 10 point havers on the server", value=leaderboard_text)
+            await ctx.send(embed=embed)
+        except Exception as ex:
+            print(ex)
+            await ctx.send(ex)
 
 async def setup(bot):
     settings = Settings()
