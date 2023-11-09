a6a8476a931f73d67af5d5f73766757453f2ca57
adiljahouh <a_jahouh@hotmail.com>
2023-11-09 16:01:16 +0100

    remove junk

--- src/commands/points.py
+++ src/commands/points.py
@@ -73,46 +73,6 @@ class PointCommands(commands.Cog):
                                   description=f"{message}",
                                   color=0xFF0000)
             await ctx.send(embed=embed)
-    # def compare_dicts_and_create_text(self, dict1, dict2)-> tuple:
-    #     cross_emoji = "❌"
-    #     check_emoji = "✅"
-    #     result_text = ""
-        
-    #     # Initialize a flag to track if all values match
-    #     all_values_match = True
-
-    #     # Compare the dictionaries
-    #     for key in dict1:
-    #         if key in dict2:
-    #             if isinstance(dict1[key], list) and isinstance(dict2[key], list):
-    #                 # Both values are lists, compare items
-    #                 matching_items = [f"{check_emoji} {item}" for item in dict1[key] if item in dict2[key]]
-    #                 non_matching_items = [f"{cross_emoji} {item}" for item in dict1[key] if item not in dict2[key]]
-    #                 if non_matching_items:
-    #                     all_values_match = False  # Set flag to False if there are non-matching items
-    #                 items_str = ' '.join(matching_items + non_matching_items)
-    #                 result_text += f"{key}: {items_str}\n"
-    #             elif dict1[key] == dict2[key]:
-    #                 # Values match
-    #                 result_text += f"{key}: {check_emoji} {dict1[key]}\n"
-    #             else:
-    #                 # Values don't match
-    #                 result_text += f"{key}: {cross_emoji} {dict1[key]}\n"
-    #                 all_values_match = False
-    #         else:
-    #             # Key doesn't exist in the second dictionary
-    #             result_text += f"{key}: {cross_emoji} {dict1[key]} -> Key not found\n"
-    #             all_values_match = False
-
-    #     # Check for any extra keys in the second dictionary
-    #     for key in dict2:
-    #         if key not in dict1:
-    #             # Key doesn't exist in the first dictionary
-    #             result_text += f"{key}: {cross_emoji} Key not found -> {dict2[key]}\n"
-    #             all_values_match = False
-
-    #     # Return both the match flag and the result text
-    #     return (all_values_match, result_text.strip())
 
     @commands.command()
     @role_check
