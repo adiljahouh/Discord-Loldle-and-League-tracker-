96c3632b14f1aa3b810daaffc04c828a85ff1dbc
adiljahouh <a_jahouh@hotmail.com>
2024-12-08 18:53:15 +0100

    adjust text size

--- src/commands/utility/dead_or_alive.py
+++ src/commands/utility/dead_or_alive.py
@@ -11,14 +11,11 @@ async def get_profile_pic(member: Member):
         return None
     avatar_url = member.avatar.url  # .avatar is a shortcut for .avatar_url in discord.py v2.x+
     avatar_url= avatar_url.replace(".png", ".jpeg").replace('?size=1024', '?size=512')
-    print(avatar_url)
     async with aiohttp.ClientSession() as session:
         async with session.get(avatar_url) as resp:
             if resp.status == 200:
                 img_data = await resp.read()
                 img = Image.open(BytesIO(img_data))
-                print(f"Image mode: {img.mode}")
-                print(f"Profile picture format: {img.format}")
                 return img
             else:
                 return None
@@ -42,7 +39,7 @@ async def draw_dead_or_alive(base_image_path: str, profile_pic: Image, font_path
         # Paste the profile picture inside the transparent block
     base_image.paste(profile_pic, (82, 266))
     # Load a font
-    font = ImageFont.truetype(font_path, size=140)  # Replace with the desired font
+    font = ImageFont.truetype(font_path, size=70)  # Replace with the desired font
 
     sample_color = base_image.getpixel((80, 150))
     text = f"BOUNTY ${1000*lifetimestrikes}"
@@ -55,7 +52,7 @@ async def draw_dead_or_alive(base_image_path: str, profile_pic: Image, font_path
     width, height = base_image.size
     x = (width - text_width) // 2
 
-    draw.text((x, 1900), text, fill=sample_color, font=font)
+    draw.text((x, 950), text, fill=sample_color, font=font)
     # Save the combined image to a BytesIO buffer
     buffer = BytesIO()
     base_image.save(buffer, format="JPEG")
